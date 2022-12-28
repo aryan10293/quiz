9e12a8d495727cef68b5af5e0b961a299d3c408b
aondrej ryan <aondrejryan@gmail.com>
2022-12-28 02:50:17 -0800

    completed quiz project

--- jss/getQuestion.js
+++ jss/getQuestion.js
@@ -1,26 +1,16 @@
-export default function question(data){
-    let answers = [...data['results'][0]['incorrect_answers']]
-    answers.push(data['results'][0]['correct_answer'])
-    document.getElementById('question').innerText = data['results'][0]['question']
-    function randomize(values) {
-        let index = values.length,  randomIndex;
-    
-        // While there remain elements to shuffle.
-        while (index != 0) {
-    
-        // Pick a remaining element.
-        randomIndex = Math.floor(Math.random() * index);
-        index--;
-    
-        // And swap it with the current element.
-        [values[index], values[randomIndex]] = [
-            values[randomIndex], values[index]];
-        }
-    
-        return values;
-    }
-  let arr = answers
+import randomize from "./random.js"
+
+export default function question(data,i){
+    let answers = [...data['results'][i]['incorrect_answers']]
+    answers.push(data['results'][i]['correct_answer'])
+    document.getElementById('question').innerHTML = data['results'][i]['question']
   // loop through answers to display answers
-  arr.for
-  console.log(randomize(arr)[1]);
-}
+  randomize(answers).forEach((x,a) => {
+      const ul = document.getElementById('ul')
+      let li = document.createElement('li')
+        li.innerText = `${a+1}: ${x}`
+        ul.appendChild(li)
+
+  })
+  return data
+} 
--- jss/main.js
+++ jss/main.js
@@ -1,14 +1,36 @@
 import question from "./getQuestion.js";
+const button = document.getElementById('btn')
 function getFetch(){
-    const url = 'https://opentdb.com/api.php?amount=10&category=21&difficulty=easy'
-  
+    const ul = document.getElementById('ul')
+    const url = 'https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple'
+    let score = 0
     fetch(url)
         .then(res => res.json()) // parse response as JSON
         .then(data => {
-          question(data)
+          let i = 0
+          question(data,i)
+            ul.addEventListener('click', function(e){
+              let cool = e.target.textContent.slice(3)
+              if (cool === question(data,i)['results'][i]['correct_answer']) {
+                score++
+                ul.innerText = ''
+                console.log(i)
+                i++
+                question(data,i)
+              }else if (9===i) {
+                document.getElementById('display').innerText = ''
+                alert(`Score ${score}/10`)
+              } else {
+                ul.innerText = ''
+                console.log(i)
+                i++
+                question(data,i)
+              }
+            })
         })
         .catch(err => {
             console.log(`error ${err}`)
         });
   }
   getFetch()
+
